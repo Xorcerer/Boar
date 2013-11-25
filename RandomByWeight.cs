ddeb3f1173243ad8951ecbc32d3c74427ac4ed19
Logan Zhou <xorcererzc@gmail.com>
2013-11-25 18:40:52 +0800

    Added RandomByWeight. #3.

---
+++ RandomByWeight.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using LoganZhou.Boar;
+using System.Diagnostics;
+
+namespace LoganZhou.Boar
+{
+	public class RandomByWeight<T>
+    {
+		class RangeNode
+		{
+			public static RangeNode CreateTarget(int number)
+			{
+				return new RangeNode { LowerBoundary = number, UpperBoundary = number };
+			}
+
+			public int LowerBoundary { get; set; }
+			public int UpperBoundary { get; set; }
+
+			public T Value { get; set; }
+
+			/// <summary>
+			/// Target Node, whose lower and upper boundary have the same value.
+			/// </summary>
+			/// <value><c>true</c> if this instance is target; otherwise, <c>false</c>.</value>
+			public bool IsTarget { get { return LowerBoundary == UpperBoundary; } }
+		}
+
+		class Comparer : IComparer<RangeNode>
+        {
+			public int Compare(RangeNode x, RangeNode y)
+            {
+				if (x.IsTarget)
+					return Compare(x.LowerBoundary, y.LowerBoundary, y.UpperBoundary);
+
+				if (y.IsTarget)
+					return  -1 * Compare(y.LowerBoundary, x.LowerBoundary, x.UpperBoundary);
+
+                Debug.Assert(false);
+                throw new InvalidOperationException("{0} failed to run binary search.".Format(typeof(RandomByWeight<T>)));
+            }
+
+            int Compare(int x, int lowerBound, int upperBound)
+            {
+                if (x < lowerBound)
+                    return -1;
+
+                return x > upperBound ? 1 : 0;
+            }
+        }
+
+        Random m_random = new Random();
+
+        // TODO: Better name.
+		readonly List<RangeNode> m_weights = new List<RangeNode>();
+
+        public bool Empty { get { return m_weights.Count == 0; } }
+
+		public int WeightSum { get { return m_weights.Count == 0 ? 0 : m_weights[m_weights.Count - 1].UpperBoundary; } }
+
+        public void Add(T item, int weight)
+        {
+            if (weight <= 0)
+                throw new ArgumentOutOfRangeException("weight");
+
+            int prevSum = WeightSum;
+			var rangeNode = new RangeNode
+			{
+				LowerBoundary = prevSum,
+				UpperBoundary = prevSum + weight,
+				Value = item,
+			};
+			m_weights.Add(rangeNode);
+        }
+
+        public T Next()
+        {
+            int weight = m_random.Next(1, WeightSum);
+            return GetByGivenWeight(weight);
+        }
+
+        T GetByGivenWeight(int weight)
+        {
+			var target = RangeNode.CreateTarget(weight);
+			int index = m_weights.BinarySearch(target, new Comparer());
+			return m_weights[index].Value;
+        }
+    }
+}
+
