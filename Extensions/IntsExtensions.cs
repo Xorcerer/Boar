6cbf6fb64cdc085c759db9840e9f868f509a0bef
Logan Zhou <xorcererzc@gmail.com>
2013-11-25 19:14:33 +0800

    Updates for int/long extensions.

--- Extensions/IntsExtensions.cs
+++ Extensions/IntsExtensions.cs
@@ -9,10 +9,41 @@ namespace LoganZhou.Boar
             return lowerBound <= x && x <= upperBound;
         }
 
+		public static bool Between(this uint x, uint lowerBound, uint upperBound)
+		{
+			return lowerBound <= x && x <= upperBound;
+		}
+
+		public static bool Between(this long x, long lowerBound, long upperBound)
+		{
+			return lowerBound <= x && x <= upperBound;
+		}
+
+		public static bool Between(this ulong x, ulong lowerBound, ulong upperBound)
+		{
+			return lowerBound <= x && x <= upperBound;
+		}
+
         public static int Clamp(this int x, int lowerBound, int upperBound)
         {
             return Math.Max(lowerBound, Math.Min(x, upperBound));
         }
+
+		public static uint Clamp(this uint x, uint lowerBound, uint upperBound)
+		{
+			return Math.Max(lowerBound, Math.Min(x, upperBound));
+		}
+
+		public static long Clamp(this long x, long lowerBound, long upperBound)
+		{
+			return Math.Max(lowerBound, Math.Min(x, upperBound));
+		}
+
+		public static ulong Clamp(this ulong x, ulong lowerBound, ulong upperBound)
+		{
+			return Math.Max(lowerBound, Math.Min(x, upperBound));
+		}
+
     }
 }
 
